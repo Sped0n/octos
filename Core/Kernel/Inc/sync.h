33a140fe351916ed88138b605b4e1c0d02c88cd4
Sped0n <hi@sped0n.com>
2024-12-28 20:19:53 +0800

    refactor: inline synchronization functions in `sync.h` and remove `sync.c`
    - Move semaphore and mutex function implementations from `sync.c` to `sync.h` as inline functions.
    - Add `stddef.h`, `api.h`, and `global.h` includes to `sync.h`.
    - Replace `tcb.h` include with `task.h` in `sync.h`.
    - Add `current_tcb` and `ready_list` as extern variables in `sync.h`.
    - Delete `sync.c` file as its functionality is now inlined in `sync.h`.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- Core/Kernel/Inc/sync.h
+++ Core/Kernel/Inc/sync.h
@@ -1,10 +1,12 @@
 #ifndef __SYNC_H__
 #define __SYNC_H__
 
-#include <stdint.h>
+#include <stddef.h>
 
+#include "Arch/stm32f4xx/Inc/api.h"
+#include "global.h"
 #include "list.h"
-#include "tcb.h"
+#include "task.h"
 
 typedef struct Sema {
     int32_t Count;
@@ -16,12 +18,80 @@ typedef struct Mutex {
     List_t BlockedList;
 } Mutex_t;
 
-void sema_init(Sema_t *sem, int32_t initial_count);
-void sema_acquire(Sema_t *sem);
-void sema_release(Sema_t *sem);
+extern TCB_t *current_tcb;
+extern List_t ready_list;
 
-void mutex_init(Mutex_t *mutex);
-void mutex_acquire(Mutex_t *mutex);
-void mutex_release(Mutex_t *mutex);
+MICROS_INLINE static inline void block_current_task(List_t *blocked_list) {
+    list_insert(blocked_list, &(current_tcb->StateListItem));
+    task_yield();
+}
+
+MICROS_INLINE static inline void unblock_one_task(List_t *blocked_list) {
+    ListItem_t *head = list_head(blocked_list);
+    list_remove(head);
+    list_insert(&ready_list, head);
+}
+
+MICROS_INLINE inline void sema_init(Sema_t *sema, int32_t initial_count) {
+    sema->Count = initial_count;
+    list_init(&(sema->BlockedList));
+}
+
+MICROS_INLINE static inline void sema_acquire(Sema_t *sema) {
+    ENTER_CRITICAL();
+
+    sema->Count--;
+    if (sema->Count < 0)
+        block_current_task(&(sema->BlockedList));
+
+    EXIT_CRITICAL();
+}
+
+MICROS_INLINE static inline void sema_release(Sema_t *sema) {
+    ENTER_CRITICAL();
+
+    sema->Count++;
+
+    if (sema->Count > 0) {
+        __enable_irq();
+        return;
+    }
+
+    if (sema->BlockedList.Length > 0)
+        unblock_one_task(&(sema->BlockedList));
+
+    EXIT_CRITICAL();
+}
+
+MICROS_INLINE inline void mutex_init(Mutex_t *mutex) {
+    mutex->Owner = NULL;
+    list_init(&(mutex->BlockedList));
+}
+
+MICROS_INLINE static inline void mutex_acquire(Mutex_t *mutex) {
+    ENTER_CRITICAL();
+
+    if (mutex->Owner == NULL)
+        mutex->Owner = current_tcb;
+    else if (mutex->Owner == current_tcb)
+        ;
+    else
+        block_current_task(&(mutex->BlockedList));
+
+    EXIT_CRITICAL();
+}
+
+MICROS_INLINE static inline void mutex_release(Mutex_t *mutex) {
+    if (mutex->Owner != current_tcb)
+        return;
+
+    EXIT_CRITICAL();
+
+    mutex->Owner = NULL;
+    if (mutex->BlockedList.Length > 0)
+        unblock_one_task(&(mutex->BlockedList));
+
+    EXIT_CRITICAL();
+}
 
 #endif

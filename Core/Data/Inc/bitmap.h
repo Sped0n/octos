0508d7dcc8f3fc39a4679d8ef3fe2debc7c9f394
Sped0n <hi@sped0n.com>
2025-01-13 19:09:56 +0800

    feat: add bitmap utility functions and boolean type support
    - Added `#include <stdbool.h>` to support boolean type usage.
    - Introduced a new inline function `bitmap_valid` to check if the bitmap data is not NULL.
    - Added a new inline function `bitmap_first_one` to find the position of the first set bit in the bitmap.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- Core/Data/Inc/bitmap.h
+++ Core/Data/Inc/bitmap.h
@@ -1,6 +1,7 @@
 #ifndef __BITMAP_H__
 #define __BITMAP_H__
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -15,6 +16,15 @@ typedef struct {
 } Bitmap_t;
 
 void bitmap_init(Bitmap_t *bm, uint32_t *data, size_t size);
+
+/** 
+  * @brief Check if a bitmap is valid
+  * @param bm Pointer to the bitmap structure to be checked
+  * @retval true if the bitmap is valid (data is not NULL), false otherwise
+  */
+OCTOS_INLINE static inline bool bitmap_valid(Bitmap_t *bm) {
+    return bm->data != NULL;
+}
 
 /**
   * @brief Sets a bit at the specified position in the bitmap
@@ -49,6 +59,19 @@ OCTOS_INLINE static inline void bitmap_reset(Bitmap_t *bm, uint32_t pos) {
 OCTOS_INLINE static inline int32_t bitmap_first_zero(Bitmap_t *bm) {
     for (size_t i = 0; i < (bm->size + 31) / 32; i++)
         if (bm->data[i] != ~0U) return i * 32 + __builtin_clz(~bm->data[i]);
+
+    return -1;
+}
+
+/**
+  * @brief Finds the position of first set bit (1) in the bitmap
+  * @param bm Pointer to the bitmap structure
+  * @return Position of first set bit if found, -1 if no set bit exists
+  * @note Uses compiler builtin function __builtin_clz for leading zero count
+  */
+OCTOS_INLINE static inline int32_t bitmap_first_one(Bitmap_t *bm) {
+    for (size_t i = 0; i < (bm->size + 31) / 32; i++)
+        if (bm->data[i] != 0U) return i * 32 + __builtin_clz(bm->data[i]);
 
     return -1;
 }

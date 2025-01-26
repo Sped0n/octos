a1a1c51033d7b9f71bb11cace0ac3a19d2f02885
Sped0n <hi@sped0n.com>
2025-01-26 23:20:45 +0800

    build: add IWYU pragma directives for new includes
    - Added new includes for `mqueue.h`, `sync.h`, and `task.h` with IWYU pragma directives.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- Core/Kernel/Inc/kernel.h
+++ Core/Kernel/Inc/kernel.h
@@ -2,6 +2,9 @@
 #define __KERNEL_H__
 
 #include "Kernel/Inc/utils.h"
+#include "mqueue.h"// IWYU pragma: keep
+#include "sync.h"  // IWYU pragma: keep
+#include "task.h"  // IWYU pragma: keep
 
 void kernel_launch(Quanta_t *quanta);
 
